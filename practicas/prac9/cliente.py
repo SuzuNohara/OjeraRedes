4b26ff5fee47c0cda1e6fac47308b9215e6fb7f8
SuzuNohara <suzu.nkdca@gmail.com>
2025-05-07 16:53:49 -0600

    update

--- practicas/prac9/cliente.py
+++ practicas/prac9/cliente.py
@@ -11,7 +11,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, struct.pack('b', ttl))
 
 # Set the interface (change to the correct IP of your interface)
-local_ip = '192.168.0.72'  # wlo1
+local_ip = '10.100.74.247'  # wlo1
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(local_ip))
 
 print("Escribe tus mensajes. Escribe 'salir' para terminar.")
